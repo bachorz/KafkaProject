b3003b7b443a7524200b341239b997c82b5c6738
rafal <rodzer116@gmail.com>
2022-10-10 22:41:43 +0200

    first commit

--- src/main/java/com/projectKafka/Main.java
+++ src/main/java/com/projectKafka/Main.java
@@ -1,2 +1,41 @@
-package com.projectKafka;public class Main {
+package com.projectKafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Producer<String, String> producer = createProducer();
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("producer-example", "testowy-klucz_one", "testowa wartosc one");
+        try {
+            producer.send(record).get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Poszło");
+
+    }
+
+    public static Producer<String, String> createProducer(){
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "test-producer");
+
+        return new KafkaProducer<>(properties);
+
+    }
 }
