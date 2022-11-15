885bb81f16ed58296aeebcf9aaeabc9372d043f8
rafal <rodzer116@gmail.com>
2022-11-15 11:33:21 +0100

    add serializer and partitioner

---
+++ src/main/java/com/projectKafka/GsonSerializer.java
@@ -0,0 +1,32 @@
+package com.projectKafka;
+
+import com.google.gson.Gson;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class GsonSerializer<T> implements Serializer<T> {
+
+    Gson gson = new Gson();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        Serializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public byte[] serialize(String s, T t) {
+        return gson.toJson(t).getBytes();
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, T data) {
+        return gson.toJson(data).getBytes();
+    }
+
+    @Override
+    public void close() {
+        Serializer.super.close();
+    }
+}
--- src/main/java/com/projectKafka/Main.java
+++ src/main/java/com/projectKafka/Main.java
@@ -1,39 +1,46 @@
 package com.projectKafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+import static java.lang.Thread.sleep;
+
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Producer<String, String> producer = createProducer();
+        Producer<String, Product> producer = createProducer();
 
-        ProducerRecord<String, String> record = new ProducerRecord<>("producer-example", "testowy-klucz_one", "testowa wartosc one");
+        ProducerRecord<String, Product> record = new ProducerRecord<>("new_test_topic","3key666", new Product("piwo", 5.05));
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    System.out.println("Sent message " + recordMetadata.toString());
+                }
+            });
+
+
         try {
-            producer.send(record).get();
+            sleep(10000);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println("Poszło");
-
     }
 
-    public static Producer<String, String> createProducer(){
+    public static Producer<String, Product> createProducer(){
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class);
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, "test-producer");
+        properties.put(ProducerConfig.ACKS_CONFIG, "0");
+        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, OwnPartitioner.class);
+
 
         return new KafkaProducer<>(properties);
 
